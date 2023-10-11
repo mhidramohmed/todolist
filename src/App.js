071bd8393b3398448958189e7ea70661ac75c481
mhidramohmed <mohamedmhidra28@gmail.com>
2023-10-12 00:50:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [list, setlist] = useState([])
+    const [x, setX] = useState('')
+
+    const inputref = useRef()
+
+    const newitem = { completed: false, x }
+
+    const addtodo = () => {
+        if (x) {
+            setlist([...list, newitem])
+
+            inputref.current.value = ''
+            setX('')
+        }
+    }
+
+    const delettodo = () => {
+
+        setlist([])
+
+
+
+    }
+
+    const handlecompled = (index) => {
+        const newlist = [...list];
+        newlist[index].completed = !newlist[index].completed
+        setlist(newlist)
+
+    }
+    const deleteitem = (index) => {
+        let removeItem = list.slice();
+        removeItem.splice(index, 1);
+        setlist(removeItem)
+
+
+    }
+
+    return (
+        <div className='App'>
+            <h1>To Do List</h1>
+            <div className='content'>
+                <ul>
+                    {
+                        list.map((e, index) => {
+                            return <div className='list'>
+                                <li className={e.completed ? 'done' : ''} onClick={() => handlecompled(index)}>{e.x}</li>
+                                <button className='x' onClick={() => { deleteitem(index) }}>❌</button>
+                            </div>
+                        })
+                    }
+
+                </ul>
+                <div>
+                    <input type='text' className='input' placeholder='Add new one ...' ref={inputref} onChange={e => { setX(e.target.value) }} />
+                </div>
+
+                <div>
+                    <button className='add' onClick={addtodo} >Add</button>
+                    <button className='delet' onClick={delettodo} >DeletAll</button>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
 }
 
-export default App;
+export default App
